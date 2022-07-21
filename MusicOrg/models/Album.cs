96b0431c7ec1cc0548f4ca76bdc75f75be5c0732
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-21 10:46:07 -0700

    add public class for album.

---
+++ MusicOrg/models/Album.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace MusicOrg.Models
+{
+  public class Album
+  {
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Album> _instances = new List<Album> {};
+
+    public Album (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+  }
+}
